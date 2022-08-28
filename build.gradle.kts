0b249640aa3563c6e866283092fced8276bfb135
Aleksey Terzi <aleksey.terzi@gmail.com>
2022-08-27 19:33:43 -0600

    Remove CivGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,54 +1,58 @@
-import net.civmc.civgradle.common.util.civRepo
-
 plugins {
-    `java-library`
-    `maven-publish`
-    id("net.civmc.civgradle.plugin") version "1.0.0-SNAPSHOT"
+	`java-library`
+	`maven-publish`
 }
 
-// Temporary hack:
-// Remove the root build directory
 gradle.buildFinished {
 	project.buildDir.deleteRecursively()
 }
 
 allprojects {
-	group = "net.civmc.factorymod"
-	version = "3.0.0-SNAPSHOT"
-	description = "FactoryMod"
+	group = rootProject.group
+	version = rootProject.version
+	description = rootProject.description
 }
 
 subprojects {
-	apply(plugin = "net.civmc.civgradle.plugin")
 	apply(plugin = "java-library")
 	apply(plugin = "maven-publish")
 
 	java {
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of(17))
+		toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+		withJavadocJar()
+		withSourcesJar()
+	}
+
+	tasks {
+		compileJava {
+			options.encoding = Charsets.UTF_8.name()
+			options.release.set(17)
+		}
+		processResources {
+			filteringCharset = Charsets.UTF_8.name()
+			filesMatching("**/plugin.yml") {
+				expand( project.properties )
+			}
 		}
 	}
 
 	repositories {
 		mavenCentral()
-        civRepo("CivMC/CivModCore")
-        civRepo("CivMC/NameLayer")
-        civRepo("CivMC/Citadel")
+		maven("https://nexus.civunion.com/repository/maven-public/")
 	}
 
 	publishing {
 		repositories {
 			maven {
-				name = "GitHubPackages"
-				url = uri("https://maven.pkg.github.com/CivMC/FactoryMod")
+				url = uri("https://nexus.civunion.com/repository/maven-releases/")
 				credentials {
-					username = System.getenv("GITHUB_ACTOR")
-					password = System.getenv("GITHUB_TOKEN")
+					username = System.getenv("REPO_USERNAME")
+					password = System.getenv("REPO_PASSWORD")
 				}
 			}
 		}
 		publications {
-			register<MavenPublication>("gpr") {
+			register<MavenPublication>("main") {
 				from(components["java"])
 			}
 		}
