78d771cbe1e27033a9824d6ac9d7d33bcc705568
Kali <90512509+Kali0033@users.noreply.github.com>
2023-12-27 15:03:49 +0200

    1.18 -> 1.20

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
 	paperweight {
-		paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+		paperDevBundle("1.20.1-R0.1-SNAPSHOT")
 	}
 
     compileOnly("net.civmc.civmodcore:civmodcore-paper:2.3.5:dev-all")
